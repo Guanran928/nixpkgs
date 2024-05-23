4a97fab0e04eb086a774e0b49b899c04ae973a92
Guanran Wang <guanran928@outlook.com>
2024-05-24 00:00:30 +0800

    nixos/pixivfe: init
    Co-authored-by: Sandro <sandro.jaeckel@gmail.com>

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1623,6 +1623,7 @@
   ./services/web-apps/pict-rs.nix
   ./services/web-apps/pingvin-share.nix
   ./services/web-apps/pixelfed.nix
+  ./services/web-apps/pixivfe.nix
   ./services/web-apps/plantuml-server.nix
   ./services/web-apps/plausible.nix
   ./services/web-apps/porn-vault/default.nix
